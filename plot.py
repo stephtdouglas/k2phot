3f8fe268018587b7600c0eac393c1e2cc6f5a167
stephtdouglas <stephanietdouglas@gmail.com>
2015-08-24 14:44:59 -0700

    change lc plotting to clip outliers; rough 4x4 position plot

--- plot.py
+++ plot.py
@@ -5,8 +5,13 @@ import logging, os
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib import ticker
 import astropy.io.ascii as at
 from astropy.io import fits
+import K2fov.projection as proj
+import K2fov.fov as fov
+from K2fov.K2onSilicon import angSepVincenty,getRaDecRollFromFieldnum
 
 import k2spin.plot
 
@@ -96,13 +101,21 @@ def lcs(lc_filename, epic=None):
     colors = np.array(["r", "k", "c", "m", "b", "g"])
 
     t = lcs["t"]
-    good = np.where(t>2065)[0]
 
     # Plot every lightcurve and the background level
     for i, colname in enumerate(ap_cols):
         ax = plt.subplot(num_aps, 1, i+1)
 
-        ax.plot(t[good], lcs[colname][good], ".", color=colors[i])
+        good = np.where((t>2065) & (np.isfinite(lcs[colname])==True))[0]
+
+        median = np.median(lcs[colname][good])
+        stdev = np.std(lcs[colname][good])
+        logging.info("%s %f %f", colname, median, stdev)
+
+        three_sig = np.where(abs(median - lcs[colname])<=(3*stdev))[0]
+        this_good = np.intersect1d(good, three_sig)
+
+        ax.plot(t[this_good], lcs[colname][this_good], ".", color=colors[i])
 #        ax.plot(t[good], lcs[colname.replace("flux","bkgd")][good], ".",
 #                color="Grey")
         ax.set_ylabel(colname)
@@ -163,5 +176,85 @@ def plot_xy(lc_filename, epic=None):
             plt.savefig("plot_outputs/{}_AMCpos.png".format(outfilename))
 
 
-if __name__=="__main__":
-    lcs("test_lc.csv")
+def plot_chips(ax,fieldnum):
+    """Plot the outline of the Kepler chips."""
+    ra_deg, dec_deg, scRoll_deg = getRaDecRollFromFieldnum(fieldnum)
+    ## convert from SC roll to FOV coordinates
+    ## do not use the fovRoll coords anywhere else
+    ## they are internal to this script only
+    fovRoll_deg = fov.getFovAngleFromSpacecraftRoll(scRoll_deg)
+
+    ## initialize class
+    k = fov.KeplerFov(ra_deg, dec_deg, fovRoll_deg)
+
+    raDec = k.getCoordsOfChannelCorners()
+
+    light_grey = np.array([float(248)/float(255)]*3)
+    #ph = proj.Gnomic(ra_deg, dec_deg)
+    ph = proj.PlateCaree()
+    k.plotPointing(ph,showOuts=False,plot_degrees=False,colour="k",mod3="None",
+        lw=1.5)
+
+
+def setup_k2_axes(ax,extents=None):
+    """Set up figure axes."""
+
+    ax.set_xlabel('R.A.',fontsize=16)
+    ax.set_ylabel('Dec',fontsize=16)
+
+    if extents is not None:
+        ax.set_xlim(extents[:2])
+        ax.set_ylim(extents[2:])
+
+    ax.invert_xaxis()
+    ax.minorticks_on()
+
+
+
+def plot_four(epic, coadd, maskmap, maskheader, init, coords, sources, 
+              campaign=4):
+
+    logging.info("Plot four %s", epic)
+
+    fig = plt.figure(figsize=(8,8))
+
+    # First plot coadded image
+    ax1 = plt.subplot(221)
+    stamp(coadd, maskmap, ax=ax1, cmap="gray")
+    centroids(ax1, init, coords, sources)
+
+    # Then plot DSS/SDSS image (empty for now)
+#    ax2 = plt.subplot(222)
+
+    # Then the pixel motion across the CCD
+    ax3 = plt.subplot(223)
+    divider3 = make_axes_locatable(ax3)
+    cax3 = divider3.append_axes("right", size="5%", pad=0.05)
+
+    stamp(coadd, maskmap, ax=ax3, cmap="gray")
+
+    lcs = at.read("lcs/ktwo{}-c0{}.csv".format(epic, campaign))
+
+    ax3.set_ylim(min(lcs["x"])*0.9,max(lcs["x"])*1.1)
+    ax3.set_xlim(min(lcs["y"])*0.9,max(lcs["y"])*1.1)
+
+    xyt = ax3.scatter(lcs["y"], lcs["x"], c=lcs["t"],  
+                      edgecolor="none", alpha=0.5,  
+                      vmin=np.percentile(lcs["t"], 5), 
+                      vmax=np.percentile(lcs["t"], 95),
+                      cmap="gnuplot")
+    cbar_ticks = np.asarray(np.percentile(lcs["t"],np.arange(10,100,20)),int)
+    cbar1 = fig.colorbar(xyt, cax=cax3, ticks=cbar_ticks)
+    cbar1.set_label("Time (d)")
+
+    # Then sky coordinates with the object position overlaid
+    ax4 = plt.subplot(224)
+    plot_chips(ax4, 4)
+    setup_k2_axes(ax4)
+    plt.plot(maskheader["RA_OBJ"], maskheader["DEC_OBJ"], '*', 
+             color="Purple", ms=25, alpha=0.8)
+
+    plt.suptitle("EPIC {}".format(epic), fontsize="large")
+    plt.tight_layout()
+
+    plt.savefig("plot_outputs/ktwo{}-c0{}_fourby.png".format(epic, campaign))
